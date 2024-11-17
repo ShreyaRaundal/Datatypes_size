979c800659501a83b5ddcc1d4a6efc10b457b270
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2024-11-17 08:10:10 +0530

    Add files via upload

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
+#include<limits.h>
 using namespace std;
 
 int main(){
-    cout<<"The size of int:"<<sizeof(int)<<endl;
-    cout<<"the of char: "<<sizeof(char)<<endl;
-    cout<<"The size of float: "<<sizeof(float)<<endl;
-    cout<<"The size of long: "<<sizeof(long)<<endl;
-    cout<<"The size of duble: "<<sizeof(double)<<endl;
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout<<"Size of minimum char: "<< CHAR_MIN <<endl;
+    cout<<"Size of maximum char: "<< CHAR_MAX <<endl;
 
+    cout<<"Size of int: "<<sizeof(int)<< "byte"<<endl;
+    cout<<"Size of short int: "<<sizeof(short int)<< "byte" <<endl;
+    cout<<"Size of long int: "<<sizeof(long int)<< "byte" <<endl;
+    cout<<"Size of Signed long int: "<<sizeof(signed long int)<< "byte" <<endl;
+    cout<<"Size of Unsigned long int: "<<sizeof(unsigned long int)<< "byte" <<endl;
+
+    cout<<"size of double: "<<sizeof(double)<<"byte" <<endl;
+    cout<<"Size of wchar_t: "<<sizeof(wchar_t)<< "byte"<<endl;
     return 0;
+
 }
