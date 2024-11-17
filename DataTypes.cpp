8fa0b2dbd8b23894486620c800ccb00a67a76da2
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2024-11-17 08:08:41 +0530

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"The size of int:"<<sizeof(int)<<endl;
+    cout<<"the of char: "<<sizeof(char)<<endl;
+    cout<<"The size of float: "<<sizeof(float)<<endl;
+    cout<<"The size of long: "<<sizeof(long)<<endl;
+    cout<<"The size of duble: "<<sizeof(double)<<endl;
+
+    return 0;
+}
